f528e82039a7006d3533246d3438455640ae1097
rv <rvv@duck.com>
2022-06-15 09:28:45 +0700

    z

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,14 @@ import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
+
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -24,12 +32,4 @@ export default function Home({ posts }) {
       </div>
     </div>
   );
-}
-
-// Fetch data at build time
-export async function getStaticProps() {
-  const posts = (await getPosts()) || [];
-  return {
-    props: { posts },
-  };
 }
